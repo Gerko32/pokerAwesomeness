de15b826d4057b565de3edffeaa08d3c23889845
Gerko32 <gaston.gerchkovich@gmail.com>
2013-10-17 18:32:00 -0500

    pepe

---
+++ src/poker/AI/Main.java
@@ -0,0 +1,13 @@
+package poker.AI;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sam^2 sucks");
+	}
+
+}
