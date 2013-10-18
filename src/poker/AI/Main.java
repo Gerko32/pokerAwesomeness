88ffb048d3a7ee5d4ffbefb8d94f785a38757ded
Joe0 <joe.pritzel0@gmail.com>
2013-10-17 20:03:41 -0500

    Add

--- src/poker/AI/Main.java
+++ src/poker/AI/Main.java
@@ -1,6 +1,7 @@
 package poker.AI;
 
 import poker.AI.parser.JSONParser;
+import poker.AI.strategy.impl.RandomStrategy;
 import poker.AI.valueobjects.Action;
 import poker.connection.Connection;
 
@@ -11,15 +12,15 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Sam^2 sucks");
-		Connection a=new Connection(new JSONParser());
-		try {
-			/*Action action=new Action();
-			action.s
-			a.action(new Action());*/
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		Connection a = new Connection(new JSONParser());
+		while (true) {
+			try {
+				a.action(new RandomStrategy().apply(a
+						.action(new Action("check", 0))));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
-
 }
